8f89a435e25aa19e67844453486568a1308da14d
Lorrany Sousc <llorranysousc@gmail.com>
2020-06-26 15:49:24 -0300

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,6 +1,6 @@
 '''Modifique o programa anterior de forma a mostrar o nome em formato de escada.'''
 
-nome = str(input('Digite seu nome aqui: '))
+nome = str(input('Digite seu nome: ').upper())
 tamanho = len(nome)
 for cont in range(1, tamanho+1):
     print(nome[0:cont])
